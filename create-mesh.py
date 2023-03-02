2cc7fa478669a89bc5cebe03a959baf74dc712e1
Tetsuo Koyama <tkoyama010@gmail.com>
2023-03-02 22:25:35 +0900

    Add height increment

--- create-mesh.py
+++ create-mesh.py
@@ -11,28 +11,31 @@ L = 500.0  # mm
 
 mesh = gf.Mesh("empty", 3)
 
-mesh.add_convex(
-    gf.GeoTrans("GT_PRISM(3,1)"),
-    [
+heights = np.linspace(0.0, L, 25 + 1)
+
+for i, height in enumerate(heights[1:]):
+    mesh.add_convex(
+        gf.GeoTrans("GT_PRISM(3,1)"),
         [
-            0,
-            d / 16 * np.cos(0.0),
-            d / 16 * np.cos(np.pi / 8),
-            0,
-            d / 16 * np.cos(0.0),
-            d / 16 * np.cos(np.pi / 8),
+            [
+                0,
+                d / 16 * np.cos(0.0),
+                d / 16 * np.cos(np.pi / 8),
+                0,
+                d / 16 * np.cos(0.0),
+                d / 16 * np.cos(np.pi / 8),
+            ],
+            [
+                0,
+                d / 16 * np.sin(0.0),
+                d / 16 * np.sin(np.pi / 8),
+                0,
+                d / 16 * np.sin(0.0),
+                d / 16 * np.sin(np.pi / 8),
+            ],
+            [heights[i], heights[i], heights[i], height, height, height],
         ],
-        [
-            0,
-            d / 16 * np.sin(0.0),
-            d / 16 * np.sin(np.pi / 8),
-            0,
-            d / 16 * np.sin(0.0),
-            d / 16 * np.sin(np.pi / 8),
-        ],
-        [0, 0, 0, L / 25, L / 25, L / 25],
-    ],
-)
+    )
 
 mesh.export_to_vtk("mesh.vtk", "ascii")
 
