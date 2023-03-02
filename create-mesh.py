b8d46c19bdb9b07e27ee53d90597d832f6e86fb3
Tetsuo Koyama <tkoyama010@gmail.com>
2023-03-02 22:16:34 +0900

    Fix parameter of mesh

--- create-mesh.py
+++ create-mesh.py
@@ -2,15 +2,39 @@ import getfem as gf
 import numpy as np
 import pyvista as pv
 
+###############################################################################
+
+d = 100.0  # mm
+L = 500.0  # mm
+
+###############################################################################
+
 mesh = gf.Mesh("empty", 3)
 
 mesh.add_convex(
     gf.GeoTrans("GT_PRISM(3,1)"),
-    [[0, 1, 0, 0, 1, 0], [0, 0, 1, 0, 0, 1], [0, 0, 0, 1, 1, 1]],
+    [
+        [
+            0,
+            d / 16 * np.cos(0.0),
+            d / 16 * np.cos(np.pi / 8),
+            0,
+            d / 16 * np.cos(0.0),
+            d / 16 * np.cos(np.pi / 8),
+        ],
+        [
+            0,
+            d / 16 * np.sin(0.0),
+            d / 16 * np.sin(np.pi / 8),
+            0,
+            d / 16 * np.sin(0.0),
+            d / 16 * np.sin(np.pi / 8),
+        ],
+        [0, 0, 0, L / 25, L / 25, L / 25],
+    ],
 )
 
 mesh.export_to_vtk("mesh.vtk", "ascii")
 
 m = pv.read("mesh.vtk")
-m.plot(cpos="xy")
-
+m.plot(cpos="xy", show_edges=True)
