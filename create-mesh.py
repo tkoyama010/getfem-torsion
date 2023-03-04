2e236edc720838910cb18a51746f2c531330f81a
Tetsuo Koyama <tkoyama010@gmail.com>
2023-03-04 16:16:26 +0900

    Fix create-mesh.py

--- create-mesh.py
+++ create-mesh.py
@@ -2,6 +2,8 @@ import getfem as gf
 import numpy as np
 import pyvista as pv
 
+pv.set_plot_theme("document")
+
 ###############################################################################
 
 d = 100.0  # mm
@@ -15,59 +17,69 @@ rhos = np.linspace(0.0, d / 2, 8 + 1)
 phis = np.linspace(0.0, 2.0 * np.pi, 16 + 1)
 zs = np.linspace(0.0, L, 25 + 1)
 
-for i, z in enumerate(zs[1:]):
-    for j, phi in enumerate(phis[1:]):
+rho_as = rhos[:-1]
+rho_bs = rhos[1:]
+phi_as = phis[:-1]
+phi_bs = phis[1:]
+z_as = zs[:-1]
+z_bs = zs[1:]
+
+for z_a, z_b in zip(z_as, z_bs):
+    for phi_a, phi_b in zip(phi_as, phi_bs):
         mesh.add_convex(
             gf.GeoTrans("GT_PRISM(3,1)"),
             [
                 [
                     0,
-                    d / 16 * 1 * np.cos(phis[j]),
-                    d / 16 * 1 * np.cos(phi),
+                    d / 16 * 1 * np.cos(phi_a),
+                    d / 16 * 1 * np.cos(phi_b),
                     0,
-                    d / 16 * 1 * np.cos(phis[j]),
-                    d / 16 * 1 * np.cos(phi),
+                    d / 16 * 1 * np.cos(phi_a),
+                    d / 16 * 1 * np.cos(phi_b),
                 ],
                 [
                     0,
-                    d / 16 * 1 * np.sin(phis[j]),
-                    d / 16 * 1 * np.sin(phi),
+                    d / 16 * 1 * np.sin(phi_a),
+                    d / 16 * 1 * np.sin(phi_b),
                     0,
-                    d / 16 * 1 * np.sin(phis[j]),
-                    d / 16 * 1 * np.sin(phi),
+                    d / 16 * 1 * np.sin(phi_a),
+                    d / 16 * 1 * np.sin(phi_b),
                 ],
-                [zs[i], zs[i], zs[i], z, z, z],
+                [z_a, z_a, z_a, z_b, z_b, z_b],
             ],
         )
-        for k, rho in enumerate(rhos[1:]):
+        for rho_a, rho_b in zip(rho_as, rho_bs):
             mesh.add_convex(
                 gf.GeoTrans("GT_QK(3,1)"),
                 [
                     [
-                        rhos[k] * np.cos(phis[j]),
-                        rhos[k] * np.cos(phi),
-                        rho * np.cos(phis[j]),
-                        rho * np.cos(phi),
-                        rhos[k] * np.cos(phis[j]),
-                        rhos[k] * np.cos(phi),
-                        rho * np.cos(phis[j]),
-                        rho * np.cos(phi),
+                        rho_a * np.cos(phi_a),
+                        rho_a * np.cos(phi_b),
+                        rho_b * np.cos(phi_a),
+                        rho_b * np.cos(phi_b),
+                        rho_a * np.cos(phi_a),
+                        rho_a * np.cos(phi_b),
+                        rho_b * np.cos(phi_a),
+                        rho_b * np.cos(phi_b),
                     ],
                     [
-                        rhos[k] * np.sin(phis[j]),
-                        rhos[k] * np.sin(phi),
-                        rho * np.sin(phis[j]),
-                        rho * np.sin(phi),
-                        rhos[k] * np.sin(phis[j]),
-                        rhos[k] * np.sin(phi),
-                        rho * np.sin(phis[j]),
-                        rho * np.sin(phi),
+                        rho_a * np.sin(phi_a),
+                        rho_a * np.sin(phi_b),
+                        rho_b * np.sin(phi_a),
+                        rho_b * np.sin(phi_b),
+                        rho_a * np.sin(phi_a),
+                        rho_a * np.sin(phi_b),
+                        rho_b * np.sin(phi_a),
+                        rho_b * np.sin(phi_b),
                     ],
-                    [zs[i], zs[i], zs[i], zs[i], z, z, z, z],
+                    [z_a, z_a, z_a, z_a, z_b, z_b, z_b, z_b],
                 ],
             )
 
 mesh.export_to_vtk("mesh.vtk", "ascii")
 
 m = pv.read("mesh.vtk")
-m.plot(cpos="xy", show_edges=True)
+plotter = pv.Plotter(off_screen=True)
+plotter.add_mesh(m, show_edges=True)
+plotter.show(screenshot='mesh.png')
+
