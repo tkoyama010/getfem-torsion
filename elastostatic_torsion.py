0dd94c65f764767300385a44bdc9154c7dcc81d8
Tetsuo Koyama <tkoyama010@gmail.com>
2023-03-05 18:29:26 +0900

    Add "Boundary selection"

--- elastostatic_torsion.py
+++ elastostatic_torsion.py
@@ -5,3 +5,14 @@ import getfem as gf
 
 mesh = gf.Mesh("load", "mesh.msh")
 
+###############################################################################
+# Boundary selection
+
+fb1 = mesh.outer_faces_with_direction([0.0, 0.0, 1.0], 0.01)  # Top boundary
+fb2 = mesh.outer_faces_with_direction([0.0, 0.0, -1.0], 0.01)  # Bottom boundary
+
+TOP_BOUND = 1
+BOTTOM_BOUND = 2
+
+mesh.set_region(TOP_BOUND, fb1)
+mesh.set_region(BOTTOM_BOUND, fb2)
